6fb776edd90290685ebfd3c30057b117592effce
Victor Pan <vpan14@gmail.com>
2019-10-27 15:08:48 -0400

    first heroku commit

---
+++ server/build/precache-manifest.b60b2438586fe1553fd9a1aa8cdfb373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72148a8b9e948e54a66a8236803e1f57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fc00b136490fd5e3e2f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "158aad06e3f749abf604",
+    "url": "/static/js/2.741bb5c4.chunk.js"
+  },
+  {
+    "revision": "9fc00b136490fd5e3e2f",
+    "url": "/static/js/main.ded4ab25.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
