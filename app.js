4400f4a5a2467261c5485add9b362d6ef64d806a
Condude77 <brow1325@purdue.edu>
2019-10-28 20:56:10 -0400

    Added Login Stuff from other Repo
    Added Login Stuff from other repo

--- app.js
+++ app.js
@@ -3,18 +3,53 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+//passport config
+require('./config/passport')(passport);
+
+//db config
+const db = require('./config/keys').mongoURI;
+
+//connect to db
+mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+
+//body parser
+app.use(express.urlencoded({ extended:true }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+//express session
+app.use(session({
+  secret: 'secret key',
+  resave: false,
+  saveUninitialized: false
+}));
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect flash
+app.use(flash());
+
+//global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,6 +57,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -40,25 +76,5 @@ app.get('/home', (req, res) => {
   }
 });
 
-app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
-});
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
