eee415911d1ad4764cdf0fc4e7e0221448ff4a8e
Victor Pan <vpan14@gmail.com>
2019-11-14 13:34:21 -0500

    fixed heroku stuff including react app for getStream

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77a6dc4700011c4619db3ebcae30103b.js"
+  "/precache-manifest.63fb234596f70967c6fc0608cabc4294.js"
 );
 
 self.addEventListener('message', (event) => {
